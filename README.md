a9e2c4fd2d1191863cd69f8a8f7f4bf39726f4ea
Peter Wishart <peter.wishart@zonal.co.uk>
2018-08-21 16:32:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MarkdownFaasNetcore
-simple test of Alex Ellis' //faas/sample-functions/MarkdownRender/ sample for Docker EE
+Simple version of Alex Ellis' //faas/sample-functions/MarkdownRender/ sample for Docker EE.
+
+Made to test kubernetes nodes on a mixed arm / x86 cluster.
---
+++ function/FunctionHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) {
+            var asString = Markdig.Markdown.ToHtml(input);
+            Console.WriteLine(asString);
+        }
+    }
+}
